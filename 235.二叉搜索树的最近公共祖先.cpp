a43282f939ea176500bb1a3c1eacc72981126282
jellywangjie <47019573+jellywangjie@users.noreply.github.com>
2020-06-05 22:26:12 +0800

    Add files via upload

---
+++ 235.二叉搜索树的最近公共祖先.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=235 lang=cpp
+ *
+ * [235] 二叉搜索树的最近公共祖先
+ */
+
+// @lc code=start
+ 
+class Solution {
+//     struct TreeNode {
+//      int val;
+//      TreeNode *left;
+//      TreeNode *right;
+//      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//  };
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(p->val<root->val && q->val<root->val) return lowestCommonAncestor(root->left,p,q);
+        if(p->val>root->val && q->val>root->val) return lowestCommonAncestor(root->right,p,q);
+        return root;
+    }
+};
+// @lc code=end
+
